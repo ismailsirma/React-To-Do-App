b317d726c34d261593ab3f99bd29c8dcaf6ceb6e
ismailsirma <ismailsirma@gmail.com>
2020-04-04 12:36:12 +0300

    1. added create new To Do item  method 2. added update New Item Text value method 3. changed render method

--- reactdemo1/react-to-do-list/src/App.js
+++ reactdemo1/react-to-do-list/src/App.js
@@ -12,17 +12,40 @@ export default class App extends Component
     this.state=
     {
       userName : "Erdem",
-      course : "React initial"
+      toDoItems : [{action : "Buy a flower", done : false},
+                  {action : "Do Workout", done : true},
+                  {action : "Study Programming", done : false},
+                  {action : "Call a friend", done : true}],
+      newItemText : ""
     }
   }
 
-  // fat arrows eliminates requirement for the return keyword
-  changeStateData = () =>
+  updateNewItemTextValue=(event) =>
   {
-    this.setState(
-      {userName : this.state.userName === "Erdem" ? "Ismail" : "Erdem" }
-    )
+    // event.target.value retrieves whatever input it was called on
+    this.setState({newItemText : event.target.value});
   }
+
+  // add new to do task
+  createNewToDo=() =>
+  {
+    if(!this.state.toDoItems
+              .find(item => item.action === this.state.newItemText))
+    {
+        this.setState({
+          // ... is spread operator in ES6
+          // {...this.props} spreads out the "own" enumerable properties in props
+          // as discrete properties wherever you use it.
+          // <Modal {...this.props} title='Modal heading'> would be same as:
+          // <Modal a={this.props.a} b={this.props.b} title=''>
+          
+            toDoItems : [ ...this.state.toDoItems, // all the previous values of to do items
+                        { action : this.state.newItemText, 
+                          done : false}],
+                          newItemText : ""
+        });
+    }
+  }  
  
   // render = () => {}  new style arrow function, below is old style:
   render()
@@ -30,12 +53,21 @@ export default class App extends Component
     return(
     <div>
       <h4 className="bg-primary text-white text-center p-2">
-          <button className="btn btn-danger m-2"
-            onClick={this.changeStateData}>
-            Change The State
-          </button>
-            { this.state.userName } şu kursu çalışıyor { this.state.course}
-        </h4>
+            { this.state.userName }'s  To Do List 
+            ({this.state.toDoItems.filter(t=> !t.done).length}) items remaining to do
+      </h4>
+      <div className="container-fluid">
+        <div className="m-1">
+          <input className="form-control" 
+              value = {this.newItemText}
+              onChange={this.updateNewItemTextValue} />
+
+              <button className="btn btn-danger mt-1"
+                    onClick={this.createNewToDo}>
+                    Add a New Task
+              </button>
+        </div>
+      </div>
     </div>
     )
   };
