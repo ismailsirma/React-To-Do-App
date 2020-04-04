0e2db9389c5d4b4f06b57833d641d57d1d18203c
ismailsirma <ismailsirma@gmail.com>
2020-04-04 12:37:46 +0300

    setstate function test

--- reactdemo1/demo1/src/App.js
+++ reactdemo1/demo1/src/App.js
@@ -6,12 +6,35 @@ import { directive } from '@babel/types';
 
 export default class App extends Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.state=
+    {
+      userName : "Erdem",
+      course : "React initial"
+    }
+  }
+
+  // fat arrows eliminates requirement for the return keyword
+  changeStateData = () =>
+  {
+    this.setState(
+      {userName : this.state.userName === "Erdem" ? "Ismail" : "Erdem" }
+    )
+  }
+ 
+  // render = () => {}  new style arrow function, below is old style:
   render()
   {
     return(
     <div>
-      <h4 className="bg-primary text-whşte text-center p-2">
-        Hello from React App
+      <h4 className="bg-primary text-white text-center p-2">
+          <button className="btn btn-danger m-2"
+            onClick={this.changeStateData}>
+            Change The State
+          </button>
+            { this.state.userName } şu kursu çalışıyor { this.state.course}
         </h4>
     </div>
     )
